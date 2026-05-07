278eca54ec967b2312129f4ff1b4141c4fec809b
Kristopher Wasserman <kristopherwasserman6@msn.com>
2026-05-07 17:03:28 -0500

    Add project files.

---
+++ SpringsAir Testing/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SpringsAir_Testing
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
